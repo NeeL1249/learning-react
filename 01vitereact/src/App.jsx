cf2857b75a5e1c5663c69c53ec201348399ca28d
NeeL1249 <neelmulik12@gmail.com>
2025-05-22 00:26:01 +0530

    learning how to use react with vite

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,13 @@
+import Chai from "./chai";
+
+function App() {
+  return (
+    <>
+      <h1>Chai Aur React</h1>
+      <Chai />
+      <p>React aap karlo</p>
+    </>
+  );
+}
+
+export default App;
